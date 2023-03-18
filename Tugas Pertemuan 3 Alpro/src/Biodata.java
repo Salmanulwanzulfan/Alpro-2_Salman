0378a147cc096beb89ae361c20307ac809d3f4f3
Salmanulwanzulfan <126679608+Salmanulwanzulfan@users.noreply.github.com>
2023-03-18 18:57:54 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3 Alpro/src/Biodata.java
@@ -0,0 +1,12 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Perkenalkan inilah Biodata diri saya");
+        System.out.println("NIM     : 2022330010");
+        System.out.println("Nama    : Salman Ulwan Zulfan");
+        System.out.println("Alamat  : Mustika Jaya");
+        System.out.println("E-Mail  : salmanzulf4n13@gmail.com");
+        System.out.println("No HP   : 081232374703");
+        System.out.println("Jurusan : Manajemen Informatika");
+    }
+    
+}
